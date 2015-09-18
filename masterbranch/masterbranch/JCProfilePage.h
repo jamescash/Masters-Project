992054a6ed8d499fef00362ade7f9d77f9ec34ae
james cash <jamescashdesign@gmail.com>
2015-09-18 21:37:37 +0100

    Compatible with x-code 7 and iOS 9

---
+++ masterbranch/masterbranch/JCProfilePage.h
@@ -0,0 +1,13 @@
+//
+//  JCProfilePage.h
+//  PreAmp
+//
+//  Created by james cash on 15/09/2015.
+//  Copyright (c) 2015 com.james.www. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JCProfilePage : UIViewController
+
+@end
---
+++ masterbranch/masterbranch/JCProfilePage.m
@@ -0,0 +1,37 @@
+//
+//  JCProfilePage.m
+//  PreAmp
+//
+//  Created by james cash on 15/09/2015.
+//  Copyright (c) 2015 com.james.www. All rights reserved.
+//
+
+#import "JCProfilePage.h"
+
+@interface JCProfilePage ()
+
+@end
+
+@implementation JCProfilePage
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
